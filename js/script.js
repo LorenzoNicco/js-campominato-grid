c72d86db0f2364f96f23a31bec4a5ff9a3c65b04
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-11 16:09:32 +0100

    Update struttura principale: html e css

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
             4A. Ogni cella avrà un numero progressivo da 1 a 100, da creare sfruttando il valore della variabile i del ciclo.
         5. Creare un eventlistener da assegnare alle celle per farle colorare di azzurro quando cliccate.
             5A. Al click dovremmo anche stampare un messaggio in console con il numero della cella cliccata.
+        6. Creare un eventlistener per far comparire la griglia quando si clicca play.
 */
 
 console.log("Collegamento js ok");
