01159e4d0d0a5866995f5d2231b16ec80f7c3b9d
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-11 15:41:17 +0100

    Init struttura html, add reset in css, steps in js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+    STEPS:
+        1. Creare la struttura html (main, bottone, contenitore griglia).
+        2. Stilizzare quanto creato in css.
+        3. Sempre in css, creare una classe per i singoli quadrati della griglia, da assegnare successivamente.
+        4. In js, usando un ciclo, creare le 100 celle che compongono la griglia.
+            4A. Ogni cella avrà un numero progressivo da 1 a 100, da creare sfruttando il valore della variabile i del ciclo.
+        5. Creare un eventlistener da assegnare alle celle per farle colorare di azzurro quando cliccate.
+            5A. Al click dovremmo anche stampare un messaggio in console con il numero della cella cliccata.
+*/
